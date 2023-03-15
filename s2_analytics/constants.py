62a82d0f391f4a66670bd115a6609912393501a9
Vndl <vndl>
2023-03-15 23:21:26 +0000

    fix wm bits and refactor

--- s2_analytics/constants.py
+++ s2_analytics/constants.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import List
 
 W_CHAINSAW = "Chainsaw"
 
@@ -52,3 +53,30 @@ WEAPON_MODS_DATES = [
     datetime.datetime(2023, 3, 5),
     datetime.datetime(2022, 12, 28),
 ]
+
+
+class WeaponMod:
+    def __init__(self, date: datetime.datetime):
+        self.datetime = date
+
+    def __str__(self):
+        return 'WeaponMod ' + self.datetime.strftime("%b'%y")
+
+
+class WeaponModCatalog:
+    def __init__(self, wms: List[WeaponMod]):
+        self.wms = wms
+
+    def latest(self):
+        return self.wms[0]
+
+    def previous(self):
+        return self.wms[1]
+
+
+WEAPON_MODS = [
+    WeaponMod(datetime.datetime(2023, 3, 5)),
+    WeaponMod(datetime.datetime(2022, 12, 28)),
+]
+
+WEAPON_MODS_CATALOG = WeaponModCatalog(WEAPON_MODS)
