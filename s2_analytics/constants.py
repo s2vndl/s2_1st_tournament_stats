dfa1f740115069b9c899830505d1a453f0605b1d
Vndl <vndl>
2023-03-20 23:36:03 +0000

    add another page for new wm

--- s2_analytics/constants.py
+++ s2_analytics/constants.py
@@ -56,11 +56,17 @@ WEAPON_MODS_DATES = [
 
 
 class WeaponMod:
-    def __init__(self, date: datetime.datetime):
+    def __init__(self, date: datetime.datetime, changes: str):
+        self.changes = changes
         self.datetime = date
 
     def __str__(self):
-        return 'WeaponMod ' + self.datetime.strftime("%b'%y")
+        return 'WeaponMod ' + self._custom_strftime("{S} %b '%y", self.datetime)
+
+    def _custom_strftime(self, format, t):
+        def _suffix(d):
+            return 'th' if 11 <= d <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(d % 10, 'th')
+        return t.strftime(format).replace('{S}', str(t.day) + _suffix(t.day))
 
 
 class WeaponModCatalog:
@@ -70,13 +76,64 @@ class WeaponModCatalog:
     def latest(self):
         return self.wms[0]
 
-    def previous(self):
-        return self.wms[1]
+    def previous(self, n=1):
+        return self.wms[n]
 
 
 WEAPON_MODS = [
-    WeaponMod(datetime.datetime(2023, 3, 5)),
-    WeaponMod(datetime.datetime(2022, 12, 28)),
+    WeaponMod(datetime.datetime(2023, 3, 20), """
+        - Rocket Launcher has lower kickback ("self-push")
+        - Rocket Launcher has lower bullet force (impact when hitting other players)
+        - Increased minimum dropkick ("headbutt") velocity in attempt to fix low-velocity player collision/bumping
+        - Steyr &  Kalashnikov have a bit more bullet force
+        """),
+    WeaponMod(datetime.datetime(2023, 3, 5), """
+    - Dragunov
+        faster interval (0.7s -> 0.67s)
+        Clip increased (6 -> 7)
+        Reloadtime decreased (2.7s -> 2.5s)
+        15% more head damage
+        12% more leg damage
+        better accuracy & less movement inaccuracy 
+
+    - Spas-12
+        less self-boost
+        faster reload (0.45s -> 0.4s)
+
+    - M79
+        boost forces decreased
+        less splash damage
+        slightly more reloadtime (2.9s -> 3.0s)
+
+    - RPG
+        boost forces decreased
+
+    - Kalashnikov & Steyr-AUG
+        more bullet penetration
+
+    - MP5
+        slightly less head damage
+        slightly less leg damage
+        Reloadtime increased (1.9s -> 2.0s)
+
+    - Barret
+        no longer 'autoreloads' when not equipped
+
+    - Makarov
+        slightly lower reloadtime (1.4s -> 1.3s)
+
+    - Knife
+        no random kills from nearby explosions
+
+    - Minigun
+        Clip increased (90 -> 100)
+
+    - Flag forces
+        various improvements"""),
+    WeaponMod(datetime.datetime(2022, 12, 28), """
+    - Mp5, Steyr, AK damage reduced
+    - Body-part damage modifiers added
+    """),
 ]
 
 WEAPON_MODS_CATALOG = WeaponModCatalog(WEAPON_MODS)
