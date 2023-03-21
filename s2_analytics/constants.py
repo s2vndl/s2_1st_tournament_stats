08c1bee51a8f65fe2da1ce21833f99dc72c5efb2
Vndl <vndl>
2023-03-21 02:48:34 +0000

    improve wm changesets

--- s2_analytics/constants.py
+++ s2_analytics/constants.py
@@ -56,8 +56,9 @@ WEAPON_MODS_DATES = [
 
 
 class WeaponMod:
-    def __init__(self, date: datetime.datetime, changes: str):
-        self.changes = changes
+    def __init__(self, date: datetime.datetime, changes: str, changes_url):
+        self.changes_url = changes_url
+        self.changes = changes.strip()
         self.datetime = date
 
     def __str__(self):
@@ -77,58 +78,62 @@ class WeaponModCatalog:
 
 WEAPON_MODS = [
     WeaponMod(datetime.datetime(2023, 3, 20), """
-        - Rocket Launcher has lower kickback ("self-push")
-        - Rocket Launcher has lower bullet force (impact when hitting other players)
-        - Increased minimum dropkick ("headbutt") velocity in attempt to fix low-velocity player collision/bumping
-        - Steyr &  Kalashnikov have a bit more bullet force
-        """),
+- RocketLauncher has lower kickback ("self-push")
+- RocketLauncher has lower bullet force (impact when hitting other players)
+- Increased minimum dropkick ("headbutt") velocity in attempt to fix low-velocity player collision/bumping
+- Steyr &  Kalashnikov have a bit more bullet force
+""", ""),
     WeaponMod(datetime.datetime(2023, 3, 5), """
-    - Dragunov
-        faster interval (0.7s -> 0.67s)
-        Clip increased (6 -> 7)
-        Reloadtime decreased (2.7s -> 2.5s)
-        15% more head damage
-        12% more leg damage
-        better accuracy & less movement inaccuracy 
+- Dragunov
+    - faster interval (0.7s -> 0.67s)
+    - Clip increased (6 -> 7)
+    - Reloadtime decreased (2.7s -> 2.5s)
+    - 15% more head damage
+    - 12% more leg damage
+    - better accuracy & less movement inaccuracy 
 
-    - Spas-12
-        less self-boost
-        faster reload (0.45s -> 0.4s)
+- Spas-12
+    - less self-boost
+    - faster reload (0.45s -> 0.4s)
 
-    - M79
-        boost forces decreased
-        less splash damage
-        slightly more reloadtime (2.9s -> 3.0s)
+- M79
+    - boost forces decreased
+    - less splash damage
+    - slightly more reloadtime (2.9s -> 3.0s)
 
-    - RPG
-        boost forces decreased
+- RPG
+    - boost forces decreased
 
-    - Kalashnikov & Steyr-AUG
-        more bullet penetration
+- Kalashnikov & Steyr-AUG
+    - more bullet penetration
 
-    - MP5
-        slightly less head damage
-        slightly less leg damage
-        Reloadtime increased (1.9s -> 2.0s)
+- MP5
+    - slightly less head damage
+    - slightly less leg damage
+    - ReloadTime increased (1.9s -> 2.0s)
 
-    - Barret
-        no longer 'autoreloads' when not equipped
+- Barret
+    - no longer 'autoreloads' when not equipped
 
-    - Makarov
-        slightly lower reloadtime (1.4s -> 1.3s)
+- Makarov
+    - slightly lower reloadtime (1.4s -> 1.3s)
 
-    - Knife
-        no random kills from nearby explosions
+- Knife
+    - no random kills from nearby explosions
 
-    - Minigun
-        Clip increased (90 -> 100)
+- Minigun
+    - clip increased (90 -> 100)
 
-    - Flag forces
-        various improvements"""),
+- Flag forces
+    various improvements
+    """, "https://www.diffchecker.com/6mF7J5Ic/"),
     WeaponMod(datetime.datetime(2022, 12, 28), """
-    - Mp5, Steyr, AK damage reduced
-    - Body-part damage modifiers added
-    """),
+- Added head, legs and torso damage modifiers.
+- Increased penetration for Barrett and Rheinmetall.
+- Added penetration to Deagles, Steyr, Ak, Dragunov, Barret and Rheinmetall.
+- Reduced flag push for most of the weapons.
+- Reduced RPG and RocketLauncher self-boost force.
+""", "https://www.diffchecker.com/Twp3SjZv/"),
 ]
 
 WEAPON_MODS_CATALOG = WeaponModCatalog(WEAPON_MODS)
